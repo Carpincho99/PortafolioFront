93717dce82b49be36084f4e341d37d3b62d441b4
Carpincho99 <ezegiorgis@hotmail.com>
2022-12-05 14:45:26 -0300

    add sobreMi component and scrollspy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { SobreMiComponent } from './sobre-mi/sobre-mi.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
+    NavBarComponent,
+    SobreMiComponent
   ],
   imports: [
     BrowserModule
