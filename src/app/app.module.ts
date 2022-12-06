e961ad26dae803b26b2cc8d7a7f8cb926d71cd2e
Carpincho99 <ezegiorgis@hotmail.com>
2022-12-06 18:23:51 -0300

    add educacion, experiencia, and init skils

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { SobreMiComponent } from './sobre-mi/sobre-mi.component';
+import { ExperienciaComponent } from './experiencia/experiencia.component';
+import { EducacionComponent } from './educacion/educacion.component';
+import { SkillsComponent } from './skills/skills.component';
+import { CircleProgressBarComponent } from './circle-progress-bar/circle-progress-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
-    SobreMiComponent
+    SobreMiComponent,
+    ExperienciaComponent,
+    EducacionComponent,
+    SkillsComponent,
+    CircleProgressBarComponent
   ],
   imports: [
     BrowserModule
